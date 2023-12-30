7c5217e16c4ddb8655573357fb55f61b88373681
Tooty582 <zing706@aol.com>
2023-12-30 17:38:42 -0500

    Automatic fullscreen and orientation lock (to be toggleable in future settings menu)

--- modules/input.js
+++ modules/input.js
@@ -14,31 +14,38 @@ export class Input{
         RightStickPosX: "turnRight"
     };
 
+    static DEAD_ZONE = 0.1;
+
     constructor(mapping){
         this.mapping = mapping || Object.assign({}, Input.defaultMapping);
         this.turnNum = 0;
+        this.didInput = false;
 
         addEventListener("keydown", (e) => {
             if(this.mapping[e.code]){
                 this[this.mapping[e.code]] = 1;
+                this.didInput = true;
             }
         });
 
         addEventListener("keyup", (e) => {
             if(this.mapping[e.code]){
                 this[this.mapping[e.code]] = 0;
+                this.didInput = true;
             }
         });
 
         addEventListener("mousemove", (e) => {
             if(document.pointerLockElement){
                 this.turnNum += e.movementX;
+                this.didInput = true;
             }
         }, true);
 
         addEventListener("click", (e) =>{
             if(!document.pointerLockElement){
                 document.body.requestPointerLock({unadjustedMovement: true});
+                this.didInput = true;
             }else{
 
             }
@@ -55,18 +62,24 @@ export class Input{
                 this.gamepad = null;
             }
         })
+
+        addEventListener("fullscreenchange", (e) => {
+            if(document.fullscreenElement){
+                screen.orientation.lock("landscape");
+            }else{
+                screen.orientation.unlock();
+            }
+        })
     }
 
     update(){
-
-
         if(this.gamepad != null){
             let controllerInput = {};
             let gamepad = navigator.getGamepads()[this.gamepad];
 
             for(let i = 0; i < gamepad.axes.length; i++){
                 let val = gamepad.axes[i];
-                if(Math.abs(val) < 0.1) val = 0;
+                if(Math.abs(val) < Input.DEAD_ZONE) val = 0;
                 let axis = ((i % 2 == 0)) && "X" || "Y";
                 let thumbstick = ((i - i % 2) / 2 == 0) && "Left" || "Right";
                 let positive = (val >= 0) && "Pos" || "Neg";
@@ -77,14 +90,27 @@ export class Input{
             }
 
             for(let i = 0; i < gamepad.buttons.length; i++){
-                controllerInput["Button" + i] = gamepad.buttons[i].value;
+                if(gamepad.buttons[i].value > Input.DEAD_ZONE){
+                    controllerInput["Button" + i] = gamepad.buttons[i].value;
+                }else{
+                    controllerInput["Button" + i] = 0;
+                }
             }
 
             for(let [k, v] of Object.entries(controllerInput)){
                 if(this.mapping[k]){
                     this[this.mapping[k]] = v;
+                    if(v > 0){
+                        this.didInput = true;
+                    }
                 }
             }
+        }
+
+        if(this.didInput){
+            if(!document.fullscreenElement) document.body.requestFullscreen();
+            if(!document.fullscreenElement) document.body.webkitRequestFullscreen();
+            this.didInput = false;
         }
     }
 }
