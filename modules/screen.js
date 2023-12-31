06a4fdf24c6edd2756b59f3292c8e237cd400671
Tooty582 <zing706@aol.com>
2023-12-30 20:52:59 -0500

    Cleaned up helper functions

--- modules/screen.js
+++ modules/screen.js
@@ -1,176 +1,176 @@
 import { Vector2 } from "./vector2.js";
 import { Sprite } from "./sprite.js";
 
-export class Screen{
-    static modulus(n, m){
-        return (n % m + m) % m
-    }
+function modulus(n, m){
+    return (n % m + m) % m
+}
 
-    static drawMapPixel(image, x, y, width, height, map, renderHeight, hitQueue, queueNum){
-        let hitEntry = hitQueue[queueNum];
-        let posX = hitEntry.posX;
-        let posY = hitEntry.posY;
-        let curPos = hitEntry.curPos;
-        let dir = hitEntry.dir;
-        let hitNorm = hitEntry.hitNorm;
-        let hitRight = hitEntry.hitRight;
-        let camPos = hitEntry.camPos;
-        let camForward = hitEntry.camForward;
+function drawMapPixel(image, x, y, width, height, map, renderHeight, hitQueue, queueNum){
+    let hitEntry = hitQueue[queueNum];
+    let posX = hitEntry.posX;
+    let posY = hitEntry.posY;
+    let curPos = hitEntry.curPos;
+    let dir = hitEntry.dir;
+    let hitNorm = hitEntry.hitNorm;
+    let hitRight = hitEntry.hitRight;
+    let camPos = hitEntry.camPos;
+    let camForward = hitEntry.camForward;
 
-        let dist = curPos.subtract(camPos).length();
-        dist = dist * camForward.dot(dir);
-        let wallHeight = width / dist;
-        let start = height / 2 - wallHeight * (1 - renderHeight);
-        let wall = map["wall" + map.walls[posX][posY]];
+    let dist = curPos.subtract(camPos).length();
+    dist = dist * camForward.dot(dir);
+    let wallHeight = width / dist;
+    let start = height / 2 - wallHeight * (1 - renderHeight);
+    let wall = map["wall" + map.walls[posX][posY]];
 
-        if(y >= start && y < start + wallHeight){
-            let wallY = (y - start) / wallHeight;
-            let wallX = Screen.modulus(curPos.dot(hitRight), 1);
-            
-            let color = 0;
+    if(y >= start && y < start + wallHeight){
+        let wallY = (y - start) / wallHeight;
+        let wallX = modulus(curPos.dot(hitRight), 1);
+        
+        let color = 0;
 
-            if(typeof(wall) == "number"){
-                color = wall;
-            }else if(typeof(wall) == "function"){
-                color = wall(wallX, wallY);
-            }else if(wall instanceof Sprite){
-                let pixelX = wall.spriteSize * wallX;
-                let pixelY = wall.spriteSize * wallY;
-                pixelX -= pixelX % 1;
-                pixelY -= pixelY % 1;
-                color = wall.getPixel(pixelX, pixelY);
-            }
-
-            let wallShade = 1;
-            if(map.wallLightVec){
-                wallShade = map.wallLightVec.dot(hitNorm) * -0.5 + 0.5;
-            }
-            let darkShade = map.wallDarkShade || 0;
-            let lightShade = map.wallLightShade || 1;
-
-            let r = (color >> 24) & 0xFF;
-            let g = (color >> 16) & 0xFF;
-            let b = (color >> 8) & 0xFF;
-
-            let portal = map.portals[posX + " " + posY];
-            if(portal && portal.normal.equals(hitNorm)){
-                let pixelX = portal.sprite.spriteSize * wallX;
-                let pixelY = portal.sprite.spriteSize * wallY;
-                pixelX -= pixelX % 1;
-                pixelY -= pixelY % 1;
-                let portalColor = portal.sprite.getPixel(pixelX, pixelY);
-                if(portalColor != portal.maskColor){
-                    let portalR = (portalColor >> 24) & 0xFF;
-                    let portalG = (portalColor >> 16) & 0xFF;
-                    let portalB = (portalColor >> 8) & 0xFF;
-                    let portalA = (portalColor & 0xFF) / 255;
-                    
-                    let modPortalA = -0.56 / (portalA - 1.4) - 0.4;
-                    portalA = portalA + (modPortalA - portalA) * (Math.cos(Date.now() / 1000 * 2 * Math.PI) + 1) / 2;
-
-                    r = portalR * portalA + r * (1 - portalA);
-                    g = portalG * portalA + g * (1 - portalA);
-                    b = portalB * portalA + b * (1 - portalA);
-
-                    r -= r % 1;
-                    g -= g % 1;
-                    b -= b % 1;
-                }else if(portal.linkedPortal){
-                    Screen.drawMapPixel(image, x, y, width, height, map, renderHeight, hitQueue, queueNum + 1);
-                    return;
-                }
-            }
-
-            r *= (darkShade + (lightShade - darkShade) * wallShade);
-            g *= (darkShade + (lightShade - darkShade) * wallShade);
-            b *= (darkShade + (lightShade - darkShade) * wallShade);
-
-            let fogNum = (dist - map.fogNear) / (map.fogFar - map.fogNear);
-            if(fogNum > 1) fogNum = 1;
-            if(fogNum < 0) fogNum = 0;
-
-            image[(y * width + x) * 4] = r * (1 - fogNum) + ((map.fogColor >> 24) & 0xFF) * fogNum;
-            image[(y * width + x) * 4 + 1] = g * (1 - fogNum) + ((map.fogColor >> 16) & 0xFF) * fogNum;
-            image[(y * width + x) * 4 + 2] = b * (1 - fogNum) + ((map.fogColor >> 8) & 0xFF) * fogNum;
-            image[(y * width + x) * 4 + 3] = 255;
-        }else if(y > height / 2){
-            let dist = (width * renderHeight - 1) / (y - height / 2);
-            let floorPos = camPos.add(dir.multiply(dist / camForward.dot(dir)));
-            let floorX = floorPos.x % 1;
-            let floorY = floorPos.y % 1;
-            let floor = map["floor" + map.floors[floorPos.x - floorX][floorPos.y - floorY]]
-
-            let color = 0
-
-            if(typeof(floor) == "number"){
-                color = floor;
-            }else if(typeof(floor) == "function"){
-                color = floor(floorX, floorY);
-            }else if(floor instanceof Sprite){
-                let pixelX = floor.spriteSize * floorX;
-                let pixelY = floor.spriteSize * floorY;
-                pixelX -= pixelX % 1;
-                pixelY -= pixelY % 1;
-                color = floor.getPixel(pixelX, pixelY);
-            }
-
-            let r = ((color >> 24) & 0xFF);
-            let g = ((color >> 16) & 0xFF);
-            let b = ((color >> 8) & 0xFF);
-
-            r *= (map.floorShade || 1);
-            g *= (map.floorShade || 1);
-            b *= (map.floorShade || 1);
-
-            let fogNum = (dist - map.fogNear) / (map.fogFar - map.fogNear);
-            if(fogNum > 1) fogNum = 1;
-            if(fogNum < 0) fogNum = 0;
-
-            image[(y * width + x) * 4] = r * (1 - fogNum) + ((map.fogColor >> 24) & 0xFF) * fogNum;
-            image[(y * width + x) * 4 + 1] = g * (1 - fogNum) + ((map.fogColor >> 16) & 0xFF) * fogNum;
-            image[(y * width + x) * 4 + 2] = b * (1 - fogNum) + ((map.fogColor >> 8) & 0xFF) * fogNum;
-            image[(y * width + x) * 4 + 3] = 255;
-        }else{
-            let dist = width * (1 - renderHeight) / (height / 2 - y);
-            let ceilPos = camPos.add(dir.multiply(dist / camForward.dot(dir)));
-            let ceilX = ceilPos.x % 1;
-            let ceilY = ceilPos.y % 1;
-            let ceil = map["ceil" + map.ceils[ceilPos.x - ceilX][ceilPos.y - ceilY]]
-
-            let color = 0
-
-            if(typeof(ceil) == "number"){
-                color = ceil;
-            }else if(typeof(ceil) == "function"){
-                color = ceil(ceilX, ceilY);
-            }else if(ceil instanceof Sprite){
-                let pixelX = ceil.spriteSize * ceilX;
-                let pixelY = ceil.spriteSize * ceilY;
-                pixelX -= pixelX % 1;
-                pixelY -= pixelY % 1;
-                color = ceil.getPixel(pixelX, pixelY);
-            }
-
-            let r = ((color >> 24) & 0xFF);
-            let g = ((color >> 16) & 0xFF);
-            let b = ((color >> 8) & 0xFF);
-
-            r *= (map.ceilShade || 1);
-            g *= (map.ceilShade || 1);
-            b *= (map.ceilShade || 1);
-
-            let fogNum = (dist - map.fogNear) / (map.fogFar - map.fogNear);
-            if(fogNum > 1) fogNum = 1;
-            if(fogNum < 0) fogNum = 0;
-
-            image[(y * width + x) * 4] = r * (1 - fogNum) + ((map.fogColor >> 24) & 0xFF) * fogNum;
-            image[(y * width + x) * 4 + 1] = g * (1 - fogNum) + ((map.fogColor >> 16) & 0xFF) * fogNum;
-            image[(y * width + x) * 4 + 2] = b * (1 - fogNum) + ((map.fogColor >> 8) & 0xFF) * fogNum;
-            image[(y * width + x) * 4 + 3] = 255;
+        if(typeof(wall) == "number"){
+            color = wall;
+        }else if(typeof(wall) == "function"){
+            color = wall(wallX, wallY);
+        }else if(wall instanceof Sprite){
+            let pixelX = wall.spriteSize * wallX;
+            let pixelY = wall.spriteSize * wallY;
+            pixelX -= pixelX % 1;
+            pixelY -= pixelY % 1;
+            color = wall.getPixel(pixelX, pixelY);
         }
-    }
 
-    static render(context, camera, map, fov, renderHeight){
+        let wallShade = 1;
+        if(map.wallLightVec){
+            wallShade = map.wallLightVec.dot(hitNorm) * -0.5 + 0.5;
+        }
+        let darkShade = map.wallDarkShade || 0;
+        let lightShade = map.wallLightShade || 1;
+
+        let r = (color >> 24) & 0xFF;
+        let g = (color >> 16) & 0xFF;
+        let b = (color >> 8) & 0xFF;
+
+        let portal = map.portals[posX + " " + posY];
+        if(portal && portal.normal.equals(hitNorm)){
+            let pixelX = portal.sprite.spriteSize * wallX;
+            let pixelY = portal.sprite.spriteSize * wallY;
+            pixelX -= pixelX % 1;
+            pixelY -= pixelY % 1;
+            let portalColor = portal.sprite.getPixel(pixelX, pixelY);
+            if(portalColor != portal.maskColor){
+                let portalR = (portalColor >> 24) & 0xFF;
+                let portalG = (portalColor >> 16) & 0xFF;
+                let portalB = (portalColor >> 8) & 0xFF;
+                let portalA = (portalColor & 0xFF) / 255;
+                
+                let modPortalA = -0.56 / (portalA - 1.4) - 0.4;
+                portalA = portalA + (modPortalA - portalA) * (Math.cos(Date.now() / 1000 * 2 * Math.PI) + 1) / 2;
+
+                r = portalR * portalA + r * (1 - portalA);
+                g = portalG * portalA + g * (1 - portalA);
+                b = portalB * portalA + b * (1 - portalA);
+
+                r -= r % 1;
+                g -= g % 1;
+                b -= b % 1;
+            }else if(portal.linkedPortal){
+                drawMapPixel(image, x, y, width, height, map, renderHeight, hitQueue, queueNum + 1);
+                return;
+            }
+        }
+
+        r *= (darkShade + (lightShade - darkShade) * wallShade);
+        g *= (darkShade + (lightShade - darkShade) * wallShade);
+        b *= (darkShade + (lightShade - darkShade) * wallShade);
+
+        let fogNum = (dist - map.fogNear) / (map.fogFar - map.fogNear);
+        if(fogNum > 1) fogNum = 1;
+        if(fogNum < 0) fogNum = 0;
+
+        image[(y * width + x) * 4] = r * (1 - fogNum) + ((map.fogColor >> 24) & 0xFF) * fogNum;
+        image[(y * width + x) * 4 + 1] = g * (1 - fogNum) + ((map.fogColor >> 16) & 0xFF) * fogNum;
+        image[(y * width + x) * 4 + 2] = b * (1 - fogNum) + ((map.fogColor >> 8) & 0xFF) * fogNum;
+        image[(y * width + x) * 4 + 3] = 255;
+    }else if(y > height / 2){
+        let dist = (width * renderHeight - 1) / (y - height / 2);
+        let floorPos = camPos.add(dir.multiply(dist / camForward.dot(dir)));
+        let floorX = floorPos.x % 1;
+        let floorY = floorPos.y % 1;
+        let floor = map["floor" + map.floors[floorPos.x - floorX][floorPos.y - floorY]]
+
+        let color = 0
+
+        if(typeof(floor) == "number"){
+            color = floor;
+        }else if(typeof(floor) == "function"){
+            color = floor(floorX, floorY);
+        }else if(floor instanceof Sprite){
+            let pixelX = floor.spriteSize * floorX;
+            let pixelY = floor.spriteSize * floorY;
+            pixelX -= pixelX % 1;
+            pixelY -= pixelY % 1;
+            color = floor.getPixel(pixelX, pixelY);
+        }
+
+        let r = ((color >> 24) & 0xFF);
+        let g = ((color >> 16) & 0xFF);
+        let b = ((color >> 8) & 0xFF);
+
+        r *= (map.floorShade || 1);
+        g *= (map.floorShade || 1);
+        b *= (map.floorShade || 1);
+
+        let fogNum = (dist - map.fogNear) / (map.fogFar - map.fogNear);
+        if(fogNum > 1) fogNum = 1;
+        if(fogNum < 0) fogNum = 0;
+
+        image[(y * width + x) * 4] = r * (1 - fogNum) + ((map.fogColor >> 24) & 0xFF) * fogNum;
+        image[(y * width + x) * 4 + 1] = g * (1 - fogNum) + ((map.fogColor >> 16) & 0xFF) * fogNum;
+        image[(y * width + x) * 4 + 2] = b * (1 - fogNum) + ((map.fogColor >> 8) & 0xFF) * fogNum;
+        image[(y * width + x) * 4 + 3] = 255;
+    }else{
+        let dist = width * (1 - renderHeight) / (height / 2 - y);
+        let ceilPos = camPos.add(dir.multiply(dist / camForward.dot(dir)));
+        let ceilX = ceilPos.x % 1;
+        let ceilY = ceilPos.y % 1;
+        let ceil = map["ceil" + map.ceils[ceilPos.x - ceilX][ceilPos.y - ceilY]]
+
+        let color = 0
+
+        if(typeof(ceil) == "number"){
+            color = ceil;
+        }else if(typeof(ceil) == "function"){
+            color = ceil(ceilX, ceilY);
+        }else if(ceil instanceof Sprite){
+            let pixelX = ceil.spriteSize * ceilX;
+            let pixelY = ceil.spriteSize * ceilY;
+            pixelX -= pixelX % 1;
+            pixelY -= pixelY % 1;
+            color = ceil.getPixel(pixelX, pixelY);
+        }
+
+        let r = ((color >> 24) & 0xFF);
+        let g = ((color >> 16) & 0xFF);
+        let b = ((color >> 8) & 0xFF);
+
+        r *= (map.ceilShade || 1);
+        g *= (map.ceilShade || 1);
+        b *= (map.ceilShade || 1);
+
+        let fogNum = (dist - map.fogNear) / (map.fogFar - map.fogNear);
+        if(fogNum > 1) fogNum = 1;
+        if(fogNum < 0) fogNum = 0;
+
+        image[(y * width + x) * 4] = r * (1 - fogNum) + ((map.fogColor >> 24) & 0xFF) * fogNum;
+        image[(y * width + x) * 4 + 1] = g * (1 - fogNum) + ((map.fogColor >> 16) & 0xFF) * fogNum;
+        image[(y * width + x) * 4 + 2] = b * (1 - fogNum) + ((map.fogColor >> 8) & 0xFF) * fogNum;
+        image[(y * width + x) * 4 + 3] = 255;
+    }
+}
+
+export class Screen{
+       static render(context, camera, map, fov, renderHeight){
         let width = context.canvas.width;
         let height = context.canvas.height;
 
@@ -287,7 +287,7 @@ export class Screen{
             }
 
             for(let y = 0; y < height; y++){
-                Screen.drawMapPixel(image, x, y, width, height, map, renderHeight, hitQueue, 0);
+                drawMapPixel(image, x, y, width, height, map, renderHeight, hitQueue, 0);
             }
         }
 
